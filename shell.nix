4d0e09431bab1ca63252737a3ca9354fc145805b
Albert Liu <albertymliu@gmail.com>
2025-02-02 13:06:43 -0500

    adding shell.nix

---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/e6cfe7821dc7ebf4e68014de11508d7aeb566fdc.tar.gz";
+
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    # Tools
+    typescript-language-server
+    nodePackages.prettier
+
+    # Programming Languages
+    nodejs_22
+    pnpm
+  ];
+}
